9fa747302e493cc48d655e980693783d1cc7a493
InternPruthatek <intern@pruthatek.com>
2024-09-12 09:27:21 +0530

    rest api

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.ApiOverview, name='home'),
+    path('create/', views.add_items, name='add-items'),
+    path('all/', views.view_items, name='view-items'),
+    path('update/<int:pk>/', views.update_items, name='update-items'),
+    path('item/<int:pk>/delete/', views.delete_items, name='delte-item')
+]
